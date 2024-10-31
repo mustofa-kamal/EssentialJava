789d270c03ccccd137e1d305ba86511fb8824368
mustofakamal <mustofa.kamal@gmail.com>
2024-10-31 16:35:47 -0400

    Initial commit

---
+++ src/main/java/com/essential/enums/EnumMain.java
@@ -0,0 +1,7 @@
+package com.essential.enums;
+
+public class EnumMain {
+    public static void main(String[] args) {
+       System.out.println("enum");
+    }
+}
---
+++ src/main/java/com/essential/streams/StreamMain.java
@@ -0,0 +1,7 @@
+package com.essential.streams;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        System.out.println("stream");
+}
+}
